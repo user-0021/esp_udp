576b560ea3b6dbc85343d4c02eb5631c0e53bb7c
user-0021 <nabeyamaoden@gmail.com>
2025-02-17 13:42:24 +0900

    some changes

--- esp_udp.c
+++ esp_udp.c
@@ -62,16 +62,6 @@ void udp_event_handler(void* arg, esp_event_base_t event_base,
 		default:
 			break;
 	}
-
-	if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-		wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-		ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-				 MAC2STR(event->mac), event->aid);
-	} else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-		wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-		ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d, reason=%d",
-				 MAC2STR(event->mac), event->aid, event->reason);
-	}
 }
 
 void esp_udp_init(const char* ssid,const char* pass)
--- esp_udp.h
+++ esp_udp.h
@@ -1,1 +1,3 @@
+#include once
+
 void esp_udp_init(const char* ssid,const char* pass);
